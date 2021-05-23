ff416ebe0d85b83b7f281054cae39f72da2d5c99
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-05-23 13:40:42 +0200

    added Calculator class and named all the functions

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear() { 
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        
+    }
+
+
+}
+
+
+
 const numberButtons = document.querySelectorAll(".calculator__buttons__number")
 const operationButtons = document.querySelectorAll(".calculator__buttons__operator")
 const equalsButton = document.querySelector(".calculator__buttons__equals")
