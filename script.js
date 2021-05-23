0d400eb6fcafcf5f4499fccb69cd1e0a19ad8eff
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-05-23 21:51:41 +0200

    finished the getDisplayNumber function

--- script.js
+++ script.js
@@ -67,7 +67,25 @@ class Calculator {
     }
 
     getDisplayNumber(number) {
-        return number
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split(".")[0])
+        const decimalDigits = stringNumber.split(".")[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = " "
+        } else {
+            integerDisplay = integerDigits.toLocaleString("en",  {
+                maximumFractionDigit : 0 })
+        }
+        if  (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+
+        // const floatNumber = parseFloat(number)
+        // if (isNaN(floatNumber)) return " "
+        // return floatNumber.toLocaleString("en")
     }
 
     updateDisplay() {
